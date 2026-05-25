ba9c9b56044736e188a8d904f5a3f3d8e0ec1f50
Ganesh Chandrasekar <55129970+cbsag@users.noreply.github.com>
2026-05-25 16:13:32 -0400

    Add: buy-and-sell-crypto - submission-21

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-21.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        '''
+        prices -> list of intergeres
+        p[i] is the price of the coin on day i
+            return the maximun profit you can achieve.
+                I choose one day to buy a coin and different day in the future to sell i.
+            I make not choose to make any transactiions where profi is 0
+
+
+
+        '''
+        profit1=[]
+        max=0
+        for i in range(len(prices)-1):
+            for j in range(i+1,len(prices)):
+                profit = prices[j]-prices[i]
+                if max<profit:
+                    max=profit
+                    profit1.append(profit)
+        return max
+        
