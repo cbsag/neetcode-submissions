0397d26760ba4fa55e2073065aa83a07ca786364
Ganesh Chandrasekar <55129970+cbsag@users.noreply.github.com>
2026-05-25 15:52:48 -0400

    Add: is-palindrome - submission-20

---
+++ Data Structures & Algorithms/is-palindrome/submission-20.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sent= [char.lower() for char in s if char.isalnum()]
+        clean_s= "".join(sent)
+        reverse = "".join(clean_s[::-1])
+        print(clean_s,reverse)
+        if clean_s == reverse:
+            return True
+        else:
+            return False
+            
+        
