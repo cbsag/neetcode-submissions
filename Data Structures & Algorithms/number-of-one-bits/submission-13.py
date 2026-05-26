4eedfef160b1edc0a348c22494109574ebadc2d5
Ganesh Chandrasekar <55129970+cbsag@users.noreply.github.com>
2026-05-26 16:23:42 -0400

    Add: number-of-one-bits - submission-13

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-13.py
@@ -0,0 +1,21 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        # # digits=[int(char) for char in str(n)]
+        # count =0
+
+        # while n:
+        #     if n& 1 == 1:
+        #         count+=1
+        #     n=n >>1
+        # return count
+        
+        # second approach
+        # count=0
+        # binary = bin(n)
+        # for ch in binary:
+        #     if ch in binary:
+        #         if ch=="1":
+        #             count+=1
+        # return count
+
+        return(bin(n).count("1"))
