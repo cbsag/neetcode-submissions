d36883f9ce1393e76e688665ffcf2718a0d55839
Ganesh Chandrasekar <55129970+cbsag@users.noreply.github.com>
2026-05-26 20:18:24 -0400

    Add: missing-number - submission-17

---
+++ Data Structures & Algorithms/missing-number/submission-17.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # for i in range(len(nums)+1):
+        #     # print(i,nums[i],len(nums))
+        #     if i not in nums:
+        #         return i
+        # return 
+
+        true_sum=sum(range(len(nums)+1))
+        print(true_sum)
+
+        missing_sum=sum(nums)
+        print(missing_sum)
+
+        return true_sum - missing_sum
