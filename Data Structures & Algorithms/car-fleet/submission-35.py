5ef922a3bdc4d806d9d4dcf361278702d5f0d78c
Ganesh Chandrasekar <55129970+cbsag@users.noreply.github.com>
2026-06-27 15:34:41 -0400

    Add: car-fleet - submission-35

---
+++ Data Structures & Algorithms/car-fleet/submission-35.py
@@ -0,0 +1,47 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        '''
+        two array 
+        one is position
+        one is speed ... both of size n
+        destination is at position target miles
+
+        constraints : a car cannot padd another car ahead of it
+        only catch up to the other car and drive at the same speed
+
+        car-fleet is an non empty set of cars driving at same positon and same speed 
+
+        '''
+        # car_fleet=[]
+        # Input: target = 10, position = [1,4], speed = [3,2]
+        # 1->3(10-1)/3=3
+        
+        # 4->2(10-4)/2=3
+
+        # each position check the time to target -> (taregt -position)/speed 
+        # then sort them based on closer to target:
+        # will have list of sorted time to taget from closer to farthest: this can be [()] list of tuples
+        # (1,3)->3
+        # (4,2)->3
+        # car_fleet=0
+        # position 4 time 3
+
+        # position 1 time 3
+
+        # current slowest fleet time ahead
+
+        cars=[]
+        stack=[]
+        n=len(position)
+        for i in range(n):
+            time=((target-position[i])/speed[i])
+            cars.append((position[i],time))
+        
+        cars.sort(reverse=True)
+        # print(cars)
+        for pos,time in cars:
+            if not stack or time>stack[-1]:
+                stack.append(time)
+        return len(stack)
+
+
