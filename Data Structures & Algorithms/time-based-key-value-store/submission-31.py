c07e58bda7b3c3ac1dd9af20c68353b90600e976
Ganesh Chandrasekar <55129970+cbsag@users.noreply.github.com>
2026-06-28 22:14:04 -0400

    Add: time-based-key-value-store - submission-31

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-31.py
@@ -0,0 +1,64 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store={}
+ 
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store:
+            self.store[key]=[]
+        self.store[key].append((timestamp,value)) # timestamp is firts so its easier to access in binary search
+
+
+    
+    def get(self, key: str, timestamp: int) -> str:
+
+        if key not in self.store:
+                return ""
+        values=self.store[key]
+        left=0
+        right=len(values)-1
+        res=""
+        print(values)
+
+        while left<=right:
+            mid=(left+right)//2
+            if values[mid][0]<=timestamp:
+                res=values[mid][1]
+                left=mid+1
+            else:
+                right=mid-1
+            # print(res)
+        return res
+
+        
+
+        '''
+        alice: [1,happy] [2,"something"]
+        l=0,r=0
+        mid
+
+        Inintialize the dictoanry to store keyis name and value,timestamp..
+
+        in set function I need to store the key,(value,timestamp)
+        
+        set -> alice:(happy,1)
+
+        get(alice,1) to get we chek if the timestamp exist then return the value[0] in that timestamp
+        if not we get the previous timestamp..
+
+        the conditon to do binary search is timesatmp>= previous_timestamp..
+
+
+        get(alice,2) check if 
+
+        after creating dictonry set return a dictonary after appendin a new value to it..
+
+        get()
+
+        is where we do the binary search..
+
+        so left=0 right= len(key[value])
+
+        '''
