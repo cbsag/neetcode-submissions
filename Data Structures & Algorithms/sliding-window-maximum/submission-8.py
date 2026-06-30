9296ae1b05a8c11d7ab7747ccc732c1820e094dc
Ganesh Chandrasekar <55129970+cbsag@users.noreply.github.com>
2026-06-30 17:07:42 -0400

    Add: sliding-window-maximum - submission-8

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-8.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        '''
+        brute force apparoch is to 
+        left= 0
+        res=[]
+        
+        for rigth in range(len(nums)):
+            window = right-left+1
+
+            if window>k:
+                left+=1
+                continue
+            max_in_window=max(max_in_window,nums[right])
+
+
+        '''
+        # res=[]
+        # for left in range(len(nums)-k+1):
+        #     right=left+k
+        #     window=nums[left:right]
+        #     res.append(max(window))
+        # return res
+        res=[]
+        deque = collections.deque()
+        l=r=0
+
+        while r<len(nums):
+            # pop smaller value from the deque
+            while deque and nums[deque[-1]]<nums[r]:
+                deque.pop()
+            deque.append(r)
+
+            # remove left val from window
+            if l>deque[0]:
+                deque.popleft()
+            if(r+1)>=k:
+                res.append(nums[deque[0]])
+                l+=1
+            r+=1
+        return res
