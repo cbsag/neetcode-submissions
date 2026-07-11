73a2b51eae6f6da1891c3a0e4236fc34c634c536
Ganesh Chandrasekar <55129970+cbsag@users.noreply.github.com>
2026-07-10 23:30:37 -0400

    Add: subtree-of-a-binary-tree - submission-17

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-17.py
@@ -0,0 +1,76 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:   
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        if not subRoot:
+            return True
+        if self.sameTree(root,subRoot):
+            return True
+        left = self.isSubtree(root.left,subRoot)
+        right= self.isSubtree(root.right,subRoot)
+
+        return left or right
+    
+    def sameTree(self,p,q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val!=q.val:
+            return False
+        
+        left=self.sameTree(p.left,q.left)
+        right=self.sameTree(p.right,q.right)
+
+        return left and right
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    #     if not p and not q:
+    #         return True
+    #     if not p or not q:
+    #         return False
+    #     if p.val != q.val:
+    #         return False
+    #     left=self.sameTree(p.left,q.left)
+    #     right=self.sameTree(p.right,q.right)
+    #     return left and right
+    # def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    #     if not root:
+    #         return False
+    #     if self.sameTree(root,subRoot):
+    #         return True
+    #     left = self.isSubtree(root.left,subRoot)
+    #     right = self.isSubtree(root.right,subRoot)
+
+    #     return left or right
+    # def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
